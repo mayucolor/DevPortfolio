7a8631c82bcda555735c16e1e4771a1e2dd68bb9
mayucolor <59694281+mayucolor@users.noreply.github.com>
2020-01-16 00:47:10 +0800

    Add files via upload

---
+++ action.php
@@ -0,0 +1,72 @@
+<?php
+
+    include 'classes/sql.php';
+
+    $s = new SQL();
+    session_start();
+    $id = $_SESSION['id'];
+   
+    if(isset($_POST['add'])){
+        $name = $_POST['name'];
+        $category = $_POST['categories'];
+        $price = $_POST['price'];
+        $pic = $_FILES['pic']['name'];
+        $id = $_POST['id'];
+
+        $target_dir = "upload/";
+        $target_file = $target_dir . basename($_FILES["pic"]);
+        $ans = $s->insertToTable($name,$category,$price,$pic);
+        if($ans == 1){
+            
+            move_uploaded_file($_FILES['pic'],$target_file);
+            
+            header("Location: adminmenu.php");
+        }else{
+            echo 'Error';
+        }
+    
+    }else if(isset($_POST['login'])){
+        $email = $_POST['email'];
+        $pass= $_POST['pass'];
+
+        $s->getValues($email,$pass);
+        
+    }else if(isset($_POST['submit'])){
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $email = $_POST['email'];
+        $pass = $_POST['pass'];
+
+        $s->insertUserData($fname,$lname,$email,$pass);
+
+    }else if(isset($_POST['send'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+        // echo "test";
+        $s->insertMessage($name,$email,$subject,$message);
+
+    }else if(isset($_POST['edit'])){
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $email = $_POST['email'];
+        $pass = $_POST['pass'];
+        
+        $s->editUser($fname,$lname,$email,$pass,$id);
+        
+    }else if(isset($_POST['search'])){
+        $select = $_POST['select'];
+
+        $cat = $s->searchImage($select);
+        $_SESSION["cat"] = $cat;
+        
+        header("Location:userLoginDisplay.php");
+
+    }else if($_GET['actiontype'] == 'delete'){
+        $id = $_GET['id'];
+
+        $s->deleteMessage($id);
+    }
+    
+?>
---
+++ messagedisplay.php
@@ -0,0 +1,53 @@
+<?php
+    session_start();
+    require_once 'classes/sql.php';
+
+    $message = new SQL;
+
+    $result = $message->getMessage();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Baskervville&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<div class="bg-success p-3 mb-5">
+        MESSAGE
+        <a href="adminmenu.php" class="text-right text-white text-decoration-none ml-5">ADD FURNITURE</a>
+        <a href="messagedisplay.php" class="text-right text-white text-decoration-none ml-5">MESSAGE</a>
+        <a href="logout.php" class="text-warning text-decoration-none ml-5">LOG OUT</a>
+        
+    </div>
+    <table class="table">
+        <th>ID</th>
+        <th>NAME</th>
+        <th>EMAIL</th>
+        <th>SUBJECT</th>
+        <th>MESSAGE</th>
+        <th>ACTION</th>
+        
+        <?php
+                foreach($result as $row){
+                $id = $row['id'];
+                echo"<tr>
+                        <td>".$row['id']."</td>
+                        <td>".$row['name']."</td>
+                        <td>".$row['email']."</td>
+                        <td>".$row['subject']."</td>
+                        <td>".$row['message']."</td>
+                        <td> <a href='action.php?actiontype=delete&id=$id' class='text-success'> Delete </a></td>
+                    </tr>";
+            }
+            
+        ?>
+    </table>
+    
+</body>
+</html>
